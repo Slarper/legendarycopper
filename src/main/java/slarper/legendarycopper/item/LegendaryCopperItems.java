9eea780ec66502e9ad65886deefec9277527d0d7
SpaceTeleport <naivesimple@outlook.com>
2022-04-22 01:03:47 +0800

    first release version

--- src/main/java/slarper/legendarycopper/item/LegendaryCopperItems.java
+++ src/main/java/slarper/legendarycopper/item/LegendaryCopperItems.java
@@ -1,5 +1,6 @@
 package slarper.legendarycopper.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.*;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.Identifier;
@@ -49,6 +50,7 @@ public class LegendaryCopperItems {
     public static final Item COPPER_PICKAXE;
     public static final Item COPPER_AXE;
     public static final Item COPPER_HOE;
+    public static final Item COPPER_NUGGET;
 
     static {
         COPPER_SWORD = register("copper_sword", new SwordItem(TOOL_MATERIAL_COPPER, 3, -2.4F, (new Item.Settings()).group(ItemGroup.COMBAT)));
@@ -56,5 +58,7 @@ public class LegendaryCopperItems {
         COPPER_PICKAXE = register("copper_pickaxe", new LegendaryCopperPickaxeItem(TOOL_MATERIAL_COPPER, 1, -2.8F, (new Item.Settings()).group(ItemGroup.TOOLS)));
         COPPER_AXE = register("copper_axe", new LegendaryCopperAxeItem(TOOL_MATERIAL_COPPER, 6.0F, -3.1F, (new Item.Settings()).group(ItemGroup.TOOLS)));
         COPPER_HOE = register("copper_hoe", new LegendaryCopperHoeItem(TOOL_MATERIAL_COPPER, -2, -1.0F, (new Item.Settings()).group(ItemGroup.TOOLS)));
+        COPPER_NUGGET = register("copper_nugget", new Item((new FabricItemSettings()).group(ItemGroup.MATERIALS)));
+
     }
 }
